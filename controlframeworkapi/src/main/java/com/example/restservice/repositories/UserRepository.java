81503418f49844a35650e6ad1f8d49b7d22fecb0
Henrique Maciel <henrique.maciel@celfocus.com>
2021-11-03 11:28:35 +0000

    Removed app, easier to use pre existing PoC

--- controlframeworkapi/src/main/java/com/example/restservice/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.restservice.repositories;
-
-import com.example.restservice.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, String> {
-
-    User findByEmail(String email);
-}
